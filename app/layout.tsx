7fcc89af45f7cd1778cdfdb49cda0bd61baf0e48
dropdead619 <sacrifice619@gmail.com>
2025-08-22 16:40:27 +0500

    feat: add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,21 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'AI BabySense – Smart Cry & Care Assistant for Parents',
   description: 'AI BabySense is your smart parenting assistant that helps you understand your baby’s cries, track feeding, sleep, and diapers, and get personalized parenting tips.',
+  icons: {
+    icon: [
+      { url: '/logo/favicon.ico', type: 'image/x-icon' },
+      { url: '/logo/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/logo/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/logo/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
+    other: [
+      { rel: 'android-chrome', url: '/logo/android-chrome-192x192.png', sizes: '192x192' },
+      { rel: 'android-chrome', url: '/logo/android-chrome-512x512.png', sizes: '512x512' },
+    ],
+  },
+  manifest: '/logo/site.webmanifest',
 }
 
 export default function RootLayout({
